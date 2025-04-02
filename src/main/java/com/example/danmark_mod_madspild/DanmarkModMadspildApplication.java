0821a146038587179f542a785988f9cb72b033b1
Badevand <mu4maroc@gmail.com>
2025-04-02 17:36:36 +0200

    First commit

---
+++ src/main/java/com/example/danmark_mod_madspild/DanmarkModMadspildApplication.java
@@ -0,0 +1,13 @@
+package com.example.danmark_mod_madspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanmarkModMadspildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanmarkModMadspildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/danmark_mod_madspild/DanmarkModMadspildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.danmark_mod_madspild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanmarkModMadspildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
