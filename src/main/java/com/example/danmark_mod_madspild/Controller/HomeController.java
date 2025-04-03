d675f48bd56cabc81c3674728bf5ba1f49694655
nauar123 <nauarbahaa@gmail.com>
2025-04-03 13:50:25 +0200

    Fikset mapperne

--- src/main/java/com/example/danmark_mod_madspild/Controller/HomeController.java
+++ src/main/java/com/example/danmark_mod_madspild/Controller/HomeController.java
@@ -1,17 +1,17 @@
-package com.example.danmark_mod_madspild;
+package com.example.danmark_mod_madspild.Controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class MainController
-{
+public class HomeController {
+
+
 
     @GetMapping("/")
     public String forside()
     {
         return "forside";
     }
-
-
 
 }
